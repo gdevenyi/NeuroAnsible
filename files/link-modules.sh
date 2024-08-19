661800e6d43e5af6ac7ffb0d6233cba01987c57d
Gabriel A. Devenyi <gdevenyi@gmail.com>
2024-08-18 22:33:53 -0400

    Improve link-modules

--- files/link-modules.sh
+++ files/link-modules.sh
@@ -4,6 +4,15 @@ set -euo pipefail
 
 for file in ../software/*/*/module; do
         #echo ${file}
-        mkdir -p $(basename $(dirname $(dirname ${file})))
-        ln -f --relative -s ${file} $(basename $(dirname $(dirname ${file})))/$(basename $(dirname ${file})).lua
+        modulename=$(basename $(dirname $(dirname "${file}")))
+        moduleversion=$(basename $(dirname "${file}")).lua
+        mkdir -p "${modulename}"
+        ln -f --relative -s "${file}" "${modulename}"/"${moduleversion}"
+done
+
+find . -type l -print0 | while IFS= read -r -d $'\0' file
+do
+        if [[ ! -e "${file}" ]] ; then
+            rm "${file}"
+        fi
 done
